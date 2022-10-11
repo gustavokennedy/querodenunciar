1edd3aafe774c76e9158476a45d9e494136723b3
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-10-11 16:48:04 -0300

    Cadastro e listagem de denúncias

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,25 +11,28 @@
     <link href="heroes.css" rel="stylesheet">
   </head>
   <body>
+  <?php 
+    include ("config.php");
+  ?>
+
     
   <main>
   <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
         <span class="fs-4"><b>Quero Denunciar</b></span>
       </a>
 
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Denpuncias</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Ajude</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Denúncias</a></li>
+        <li class="nav-item"><a href="#sobre" class="nav-link">Sobre</a></li>
+        <li class="nav-item"><a href="#ajude" class="nav-link">Ajude</a></li>
       </ul>
     </header>
   </div>
 </main>
 
-<div class="b-example-divider"></div>
+<div id="banner" class="b-example-divider"></div>
 
   <div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
@@ -41,16 +44,58 @@
         <p class="lead">O Quero Denunciar foi criado para facilitar o registro de representações em órgãos municipais, estaduais e federais.</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
           <a href="nova-denuncia.php"><button  class="btn btn-primary btn-lg px-4 me-md-2">Criar denúncia</button></a>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Ver todas denúncias</button>
+          <a href="lista-denuncia.php"><button type="button" class="btn btn-outline-secondary btn-lg px-4">Ver todas denúncias</button></a>
         </div>
       </div>
     </div>
   </div>
 
-  <?php 
-    include ("config.php");
-  ?>
 
+  <div id="sobre" class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1 class="display-4 fw-bold">Sobre Quero Denunciar</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">A Quero Denunciar não possui vinculo com órgãos ou empresas. É um sistema autonomo para registros de denúncias em âmbito nacional, sem nenhum patrocinador. O objetivo do sistema é facilitar as denúncias para todo o país.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+        <a href="#ajude"><button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Ajude a manter</button></a>
+      </div>
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh;">
+      <div class="container px-5">
+        <img src="bg2.png" class="img-fluid mb-4" alt="Example image" width="700" height="600" loading="lazy">
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container col-xl-10 col-xxl-8 px-4 py-5" id="ajude">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 mb-3">Ajude a manter</h1>
+        <p class="col-lg-10 fs-4">As doações são utilizadas para novos recursos e infraestrutura Cloud do sistema. Cada centavo ajuda continuar o projeto Quero Denunciar.</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-light">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+          <hr class="my-4">
+          <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+        </form>
+      </div>
+    </div>
+  </div>
+  
 
 <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
--- nova-denuncia.php
+++ nova-denuncia.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,12 +16,11 @@
   <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
         <span class="fs-4"><b>Quero Denunciar</b></span>
       </a>
 
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Denpuncias</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Denúncias</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Ajude</a></li>
       </ul>
@@ -33,51 +32,44 @@
     include ("config.php");
   ?>
 
-<div class="modal modal-sheet position-static d-block  py-5" tabindex="-1" role="dialog" id="modalSheet">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content rounded-4 shadow">
-      <div class="modal-header border-bottom-0">
-        <h1 class="modal-title fs-5">Criar Denúncia</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body py-0">
-        <p>Para registrar sua denúncia, informe os campos abaixo.</p>
-        <form action="salvar.php" method="POST">
+
+<div class="container-sm">
+  <h1 class="modal-title fs-5">Criar Denúncia</h1>
+  <p>Para registrar sua denúncia, informe os campos abaixo:</p>
+
+  <form action="salvar.php" method="POST">
             <div class="mb-3">
-                <label for="estado" class="form-label">Estado</label>
-                <input type="text" class="form-control" id="estado" name="estado" aria-describedby="estado">
+                <label for="estado" class="form-label">Estado*</label>
+                <input required type="text" class="form-control" id="estado" name="estado" aria-describedby="estado">
             </div>
             <div class="mb-3">
-                <label for="municipio" class="form-label">Municpio</label>
-                <input type="text" class="form-control" id="municipio" name="municipio" aria-describedby="municipio">
+                <label for="municipio" class="form-label">Municpio*</label>
+                <input required type="text" class="form-control" id="municipio" name="municipio" aria-describedby="municipio">
             </div>
             <div class="mb-3">
-                <label for="pessoas" class="form-label">Pessoas envolvidas</label>
-                <input type="text" class="form-control" id="pessoas" name="pessoas" aria-describedby="pessoas">
+                <label for="pessoas" class="form-label">Pessoas envolvidas*</label>
+                <input required type="text" class="form-control" id="pessoas" name="pessoas" aria-describedby="pessoas">
             </div>
             <div class="mb-3">
-                <label for="testemunhas" class="form-label">Testemunhas</label>
-                <input type="text" class="form-control" id="testemunhas" name="testemunhas" aria-describedby="testemunhas">
+                <label for="testemunhas" class="form-label">Testemunhas*</label>
+                <input required type="text" class="form-control" id="testemunhas" name="testemunhas" aria-describedby="testemunhas">
             </div>
             <div class="mb-3">
-                <label for="endereco" class="form-label">Endereço do fato</label>
-                <input type="text" class="form-control" id="endereco" name="endereco" aria-describedby="endereco">
+                <label for="endereco" class="form-label">Endereço do fato*</label>
+                <input required type="text" class="form-control" id="endereco" name="endereco" aria-describedby="endereco">
             </div>
             <div class="mb-3">
-                <label for="descricao" class="form-label">Descrição</label>
-                <input type="text" class="form-control" id="descricao" name="descricao" aria-describedby="descricao">
+                <label for="descricao" class="form-label">Descrição*</label>
+                <input required type="text" class="form-control" id="descricao" name="descricao" aria-describedby="descricao">
             </div>
        
       </div>
       <div class="modal-footer flex-column border-top-0">
-        <button type="submit" class="btn btn-lg btn-primary w-100 mx-0 mb-2">Enviar</button>
-        <button type="button" class="btn btn-lg btn-light w-100 mx-0" data-bs-dismiss="modal">Fechar</button>
+        <button type="submit" class="btn btn-lg btn-primary w-50 mx-0 mb-2">Enviar</button>
       </div>
-    </div>
-  </div>
-</div>
+      <br />
 </form>
-
+</div>
 
 <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
