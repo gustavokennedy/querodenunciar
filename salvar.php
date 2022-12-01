367e5a6f5eb7b6dc0d0adadaa5336b85aeb6fd1d
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-12-01 13:45:42 +0000

    Ajuste redirecionamento

--- salvar.php
+++ salvar.php
@@ -9,15 +9,17 @@
     $testemunhas = $_POST["testemunhas"];
     $endereco = $_POST["endereco"];
     $descricao = $_POST["descricao"];
+
+    #var local = window.location.href;
  
     $sql = "INSERT INTO representacao (id, estado, municipio, pessoas, testemunhas, endereco, descricao, data_criacao) VALUES (id, '{$estado}', '{$municipio}', '{$pessoas}', '{$testemunhas}', '{$endereco}', '{$descricao}', '{$data}')";
     $res = $conn->query($sql);
 
     if ($res ==true){
             print "<script>alert('Denuncia criada!');</script>";
-            print "<script>location.href='http://localhost';</script>";
+            print "<script>location.href='http://10.0.0.154/lista-denuncia.php';</script>";
     } else {
         print "<script>alert('Falha ao criar denúncia, verifique!');</script>";
-        print "<script>location.href='http://localhost';</script>";
+        print "<script>location.href='http://10.0.0.154';</script>";
     }
 ?>
