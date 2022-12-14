d72d528c16a737cd6b12d1cad3929d86e0765f09
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-12-14 19:14:25 +0000

    Atualiado home

--- index.php
+++ index.php
@@ -78,19 +78,11 @@
       <div class="col-md-10 mx-auto col-lg-5">
         <form class="p-4 p-md-5 border rounded-3 bg-light">
           <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+		<img src="https://bitcoin.org/img/icons/logotop.svg" width="175"><br />
+		<br />
+		Doe com Bitcoin:<br />
+		<b>ENDEREÇO</b>	
           </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
-          </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
           <hr class="my-4">
           <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
         </form>
